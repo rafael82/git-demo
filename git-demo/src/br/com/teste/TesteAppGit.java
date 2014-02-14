9a9b03c89c9ee0a29fc228501f69ec631c221108
Rafael Santos <rpereira@desconstruindo.eng.br>
2014-02-14 17:10:38 -0200

    Versão 2

--- git-demo/src/br/com/teste/TesteAppGit.java
+++ git-demo/src/br/com/teste/TesteAppGit.java
@@ -3,7 +3,7 @@ package br.com.teste;
 public class TesteAppGit {
 
 	public static void main(String[] args) {
-		System.out.println("Teste de uso com EGIT");
+		System.out.println("Teste de uso com EGIT Versão 2");
 	}
 
 }
